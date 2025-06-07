fd746ff5fc796c8bafe45e9b3c43b2cae659c183
shiva-rsl <shrasouli2@gmail.com>
2025-06-07 08:25:25 +0330

    feat(ui): enhance visual appearance of the Tkinter interface

--- tkinter_code.py
+++ tkinter_code.py
@@ -5,13 +5,15 @@ from tkinter import E,W,S,N
 
 # Main window
 window = tk.Tk()
-window.geometry('700x750')
+window.geometry('700x900')
 window.resizable(width=False, height=False)
 window.title('Random Password Generator App')
 
 window.columnconfigure(0, weight=1)
 window.columnconfigure(1, weight=2)
 window.columnconfigure(2, weight=1)
+# Optional: use a consistent background color
+window.configure(bg="#DFE4E8")
 
 
 # LabelFrames
@@ -20,18 +22,26 @@ labelframe_settings = tk.LabelFrame(
     text='Password settings', 
     font=('Noto Sans', 20, ),
 )
-labelframe_settings.grid(row=2, column=0, columnspan=50, padx=(10, 10), pady=(20, 30), sticky='EW')
+labelframe_settings.grid(row=2, column=0, columnspan=5, padx=(10, 10), pady=(20, 0), sticky='EW')
 
 labelframe_settings.columnconfigure(0, weight=1)
 labelframe_settings.columnconfigure(1, weight=1)
 labelframe_settings.columnconfigure(2, weight=1)
 
+
 labelframe_generated_password = tk.LabelFrame(
     master=window,
-    text='Generated Password',
+    text='Generated Password(s)',
     font=('Noto Sans', 20, ),
 )
-labelframe_generated_password.grid(row=3, column=0, ipadx=60, ipady=20, padx=(10, 10))
+labelframe_generated_password.grid(row=3, column=0, ipadx=120, padx=(10, 10), pady=(20, 20))
+
+
+labelframe_buttons = tk.LabelFrame(
+    master=window,
+)
+labelframe_buttons.grid(row=4, column=0, ipadx=15, ipady=0, padx=(10, 10), )
+
 
 
 # Labels
@@ -54,7 +64,7 @@ label_title = tk.Label(
     foreground='yellow', 
     font=('Noto Sans', 20)
 )
-label_title.grid(row=0, column=0, columnspan=50, ipadx=250, ipady=15, sticky='NSEW')
+label_title.grid(row=0, column=0, columnspan=5, ipadx=250, ipady=15, sticky='NSEW')
 
 
 label_title_definition = tk.Label(
@@ -64,7 +74,7 @@ label_title_definition = tk.Label(
     foreground='yellow', 
     font=('Noto Sans', 12),
 )
-label_title_definition.grid(row=1, column=0, columnspan=50, ipady=5, sticky='NEW')
+label_title_definition.grid(row=1, column=0, columnspan=5, ipady=5, sticky='NEW')
 
 
 label_password_length = tk.Label(
@@ -99,38 +109,22 @@ label_password_quantity_numbers.grid(row=1, column=2, padx=(5, 10), pady=(20, 30
 
 label_random_password = tk.Label(
     master=labelframe_generated_password, 
-    text='Your Password: ',
-    font=('Noto Sans', 10),
+    text='Your Password(s): ',
+    font=('Noto Sans', 15),
     anchor='center'
 )
-label_random_password.grid(row=0, column=1, pady=30, sticky='E', )
+label_random_password.grid(row=0, column=0, padx=20, pady=30, sticky='E', )
 
 
-# label_generated_password = tk.Label(master=window, background='yellow')
-# label_generated_password.grid()
+# texts
 
-# label_password_strength = tk.Label(master=window, text='Password strength')
-# label_password_strength.grid()
-
-# label_showing_password_strength = tk.Label(master=window,)
-# label_showing_password_strength.grid()
-
-# label_include_user_charatcers = tk.Label(master=window, text='Also include the following charatcers:')
-# label_include_user_charatcers.grid()
-
-
-
-# Entries
-entry_pass_length = tk.Entry(
+entry_generated_password = tk.Text(
     master=labelframe_generated_password,
+    height=20,
+    width=60,
+    state='disabled',
 )
-entry_pass_length.grid(row=0, column=2, ipadx=50, ipady=10, sticky='W')
-
-# entry_password_numbers = tk.Entry(master=window, )
-# entry_password_numbers.grid()
-
-# entry_include_user_characters = tk.Entry(master=window, )
-# entry_include_user_characters.grid()
+entry_generated_password.grid(row=0, column=2, )
 
 
 # Checkboxs
@@ -156,12 +150,6 @@ for index, (text, var) in enumerate(checkbox_options.items()):
     cb.grid(row=2 + row, column=col, sticky='w', padx=20, pady=5)
 
 
-# Comboboxes
-# combobox_genereted_passwords = ttk.Combobox(master=window)
-# combobox_genereted_passwords.set(' ')
-# combobox_genereted_passwords.grid()
-
-
 # Spinbox
 
 spinbox_password_length = tk.Spinbox(
@@ -184,10 +172,6 @@ spinbox_password_quantity.grid(row=1, column=1, pady=(20, 30),  ipadx=10, ipady=
 
 
 # Buttons
-# button_save = tk.Button(master=window, text='Save password')
-# button_save.grid()
-
-# image_of_key = tk.PhotoImage(file='./Pictures/key.png')
 button_generate = tk.Button(
     master=labelframe_settings, 
     text='Generate Password', 
@@ -196,30 +180,53 @@ button_generate = tk.Button(
     anchor='center',
     font=('Noto Sans', 10), 
 )
-button_generate.grid(row=6, column=1, pady=20, ipady=2, sticky='W')
+button_generate.grid(row=6, column=1, pady=20, ipady=7, sticky='W')
 
+button_save = tk.Button(
+    master=labelframe_buttons, 
+    text='Save Password(s)',
+    font=('Noto Sans', 10),
+)
+button_save.grid(row=0, column=0, ipadx=12, ipady=10)
 
-# button_copy_to_clipboard = tk.Button(master=window, text='Copy to clipboard')
-# button_copy_to_clipboard.grid()
+button_copy_to_clipboard = tk.Button(
+    master=labelframe_buttons, 
+    text='Copy to clipboard',
+    font=('Noto Sans', 10),
+)
+button_copy_to_clipboard.grid(row=0, column=1, ipadx=12, ipady=10)
 
 button_close = tk.Button(
-    master=labelframe_generated_password, 
-    text='Close', 
+    master=labelframe_buttons, 
+    text='Close',
+    font=('Noto Sans', 10),
     command=window.destroy)
-button_close.grid(row=3, column=5, ipadx=20, ipady=5, sticky=(tk.E))
+button_close.grid(row=0, column=4, ipadx=20, ipady=10)
 
-# button_about = tk.Button(master=window, text='About')
-# button_about.grid()
+button_about = tk.Button(
+    master=labelframe_buttons, 
+    text='Clear',
+    font=('Noto Sans', 10),
+)
+button_about.grid(row=0, column=2, ipadx=20, ipady=10)
 
+button_about = tk.Button(
+    master=labelframe_buttons, 
+    text='About',
+    font=('Noto Sans', 10),
+)
+button_about.grid(row=0, column=3, ipadx=20, ipady=10)
 
 
 # ProgressBar
-progressbar_generated_password = ttk.Progressbar(
-    master=labelframe_generated_password,
+# progressbar_generated_password = ttk.Progressbar(
+#     master=labelframe_generated_password,
 
-)
-progressbar_generated_password.grid(row=1, padx=5, pady=10, sticky='SNEW') 
+# )
+# progressbar_generated_password.grid(row=1, padx=5, pady=10, sticky='SNEW') 
 
+# label_password_strength = tk.Label(master=window, text='Password strength')
+# label_password_strength.grid()
 
 
 window.mainloop()
